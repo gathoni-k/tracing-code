c4110efcf7b458b3809fdba42535f68eb73042f6
gathoni-k <maryg.kahiga@gmail.com>
2022-09-10 19:03:29 +0300

    Rename

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,8 +1,7 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Date from '../Date/Date'
 import Header from '../Header/Header'
-import Pill from '../Pill/Pill'
+import NavLink from '../NavLink/NavLink'
 import Socials from '../Socials/Socials'
 import styles from './layout.module.css'
 
@@ -22,10 +21,10 @@ export default function Layout ({children}:{children:JSX.Element}) {
       </div>  
         <Header/>
         <div className={styles.categoryWrapper}>
-        <Pill text="blog" background='#deecf5' link="/" external={false}/>
-        <Pill text="about" background='#DBDBFD' link="/about" external={false}/>
-        <Pill text="resources" background='#FBE9E7' link="https://marygathoni.notion.site/51e13565f48f4981a7ce1dd5b47486de?v=675caf053db546a0bdce9133e20fbfdd" external={true}/>
-        <Pill text="projects" background='#FCD3E5' link="/projects" external={false}/>
+        <NavLink text="blog" background='#deecf5' link="/" external={false}/>
+        <NavLink text="about" background='#DBDBFD' link="/about" external={false}/>
+        <NavLink text="reading" background='#FBE9E7' link="/reading-list" external={false}/>
+        <NavLink text="projects" background='#FCD3E5' link="/projects" external={false}/>
         </div>
         {children}
       </main>
