e8538f0693740fc6c8031ec6ea42219387c7a973
gathoni-k <maryg.kahiga@gmail.com>
2022-09-20 16:17:05 +0300

    Record page views on route change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,50 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout/Layout'
-
+import Script from 'next/script'
+import { useRouter } from 'next/router';
+import { useEffect } from "react";
+import * as gtag from "../lib/ga/gtag"
+const isProd = process.env.NODE_ENV === "production";
+const GA_TRACKINGID = process.env.GA_TRACKING_ID
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      if (isProd) gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {isProd && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKINGID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKINGID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                }}
+              />
+            </>
+          )}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
