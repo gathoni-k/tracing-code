61385be2f40653287e50dfe09f91771d3e1f0449
gathoni-k <maryg.kahiga@gmail.com>
2022-09-20 16:10:33 +0300

    Add home page

---
+++ pages/blog.tsx
@@ -0,0 +1,43 @@
+import type { NextPage } from 'next'
+import ArticleCard from '../components/ArticleCard/ArticleCard'
+import { getAllPublished } from '../helpers/notion'
+import { postMetaProps } from '../lib/types'
+import styles from '../styles/Home.module.css'
+interface iPosts {
+    posts: postMetaProps[]
+}
+
+const Blog: NextPage<iPosts> = ({posts}) => {
+  if(!posts) {
+    return (
+      <div className={styles.articles}>
+        <p>Could not fetch posts:(</p>
+        <p>Check back later...</p>
+        
+      </div>
+        
+    )
+  }
+  return (
+    <div className={styles.articles}>
+      <h1 style={{fontSize: "3rem"}}>Blog</h1>
+      {posts.map((post, index) => (
+        <ArticleCard key={index} title={post.title} link={`posts/${post.slug}`} description={post.description} date={post.date}/>
+      ))}
+    </div>
+      
+  )
+}
+export const getStaticProps = async () => {
+  const data = await getAllPublished()
+    if(!data) {
+    return {
+      props: { posts: null },
+    };
+  }
+  return {
+    props: { posts: data, revalidate: 600, },
+  };
+};
+
+export default Blog
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,37 @@
 import type { NextPage } from 'next'
-import ArticleCard from '../components/ArticleCard/ArticleCard'
-import { getAllPublished } from '../helpers/notion'
-import { postMetaProps } from '../lib/types'
-import styles from '../styles/Home.module.css'
-interface iPosts {
-    posts: postMetaProps[]
-}
 
-const Home: NextPage<iPosts> = ({posts}) => {
-  if(!posts) {
-    return (
-      <div className={styles.articles}>
-        <p>Could not fetch posts:(</p>
-        <p>Check back later...</p>
-        
-      </div>
-        
-    )
-  }
+import styles from '../styles/Home.module.css'
+
+
+const Home:NextPage = () => {
   return (
     <div className={styles.articles}>
-      <h1 style={{fontSize: "3rem"}}>Blog</h1>
-      {posts.map((post, index) => (
-        <ArticleCard key={index} title={post.title} link={`posts/${post.slug}`} description={post.description} date={post.date}/>
-      ))}
+      <p style={{fontSize: "4rem",  lineHeight: "0.2rem"}}>Hi there, I&apos;m Mary.</p>
+      <p style={{fontSize: "4rem", lineHeight: "0.4rem"}}>Frontend developer and technical writer.</p>
+      <p style={{lineHeight: "2.8rem"}}>I strongly believe that accessibility should not be an afterthought and must be ingrained in the development process. 
+      This is why I strive to build components that not only solve a problem but are also accessible. </p>
+      <p>As I create my side projects, I have had to relearn how to write accessible code. It&apos;s a learning journey that I am still on.</p>
+      <section>
+        <h1>My Skills</h1>
+        <p>I am mostly immersed in the JavaScript ecosytem and most of my skills are in that area.</p>
+        <ul>
+          <li>HTML</li>
+          <li>CSS/SASS</li>
+          <li>TypeScript</li>
+          <li>React</li>
+          <li>Nextjs</li>
+          <li>Node</li>
+          <li>Git</li>
+          <li>SQL</li>
+        </ul>
+        <p>I am also continuosly learning and have taken online courses like <strong>CSS for JavaScript developers</strong>, <strong>JavaScript30</strong>, and <strong>Advanced React.</strong> I am currently taking, <strong>JavaScript the weird parts.</strong></p>
+      </section>
+      <section>
+        <h1>Projects I have worked on</h1>
+      </section>
     </div>
       
   )
 }
-export const getStaticProps = async () => {
-  const data = await getAllPublished()
-    if(!data) {
-    return {
-      props: { posts: null },
-    };
-  }
-  return {
-    props: { posts: data, revalidate: 600, },
-  };
-};
 
 export default Home
